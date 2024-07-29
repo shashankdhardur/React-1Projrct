ab8b98e035132b42ae465d17699b01cb014b7d45
shashankdhardur <165265010+shashankdhardur@users.noreply.github.com>
2024-07-29 22:19:17 +0530

    message

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h1", {}, "heading1"),
     React.createElement("h2", {}, "heading2"),
   ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "heading1"),
+    React.createElement("h2", {}, "heading2"),
+  ]),
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
