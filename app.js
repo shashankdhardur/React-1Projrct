25cef492d91cc00c15584074ad39c024f036c6e0
shashankdhardur <165265010+shashankdhardur@users.noreply.github.com>
2024-07-30 15:11:46 +0530

    new react app

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-const heading = React.createElement("h1", { id: "heading" }, " hello world");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "heading1"),
-    React.createElement("h2", {}, "heading2"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "heading1"),
-    React.createElement("h2", {}, "heading2"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "heading1"),
-    React.createElement("h2", {}, "heading2"),
-  ]),
-]);
+const heading = React.createElement("h1", { id: "heading" }, "new react app");
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(heading);
